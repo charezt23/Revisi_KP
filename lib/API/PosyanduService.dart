d7242bbfed34bedb00d6a32743f6f1f3225ef477
charezt23 <kharis.subani@gmail.com>
2025-07-05 18:27:38 +0700

    fix: error commit on posyandu services

--- lib/API/PosyanduService.dart
+++ lib/API/PosyanduService.dart
@@ -4,6 +4,28 @@ import 'package:flutter_application_1/models/posyanduModel.dart';
 import 'package:http/http.dart' as http;
 
 class Posyanduservice {
+  CreatePosyandu(String namaPosyandu, String namaDesa, int userId) async {
+    try {
+      var request = http.Request('POST', Uri.parse(base_url + '/posyandu'));
+      request.body = json.encode({
+        'nama_posyandu': namaPosyandu,
+        'nama_desa': namaDesa,
+        'user_id': userId,
+      });
+      request.headers.addAll({'Content-Type': 'application/json'});
+      http.StreamedResponse response = await request.send();
+      if (response.statusCode == 200) {
+        print('Response status: ${response.statusCode}');
+        return true;
+      } else {
+        print('Error: ${response.reasonPhrase}');
+        return false;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   GetPosyanduByUser(id) async {
     try {
       var request = http.Request(
@@ -24,6 +46,51 @@ class Posyanduservice {
       } else {
         print('Error: ${response.reasonPhrase}');
         return null;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  UpdatePosyandu(int id, String namaPosyandu, String namaDesa) async {
+    try {
+      var request = http.Request(
+        'PUT',
+        Uri.parse(base_url + '/posyandu/${id}'),
+      );
+      request.body = json.encode({
+        'nama_posyandu': namaPosyandu,
+        'nama_desa': namaDesa,
+      });
+      request.headers.addAll({'Content-Type': 'application/json'});
+      http.StreamedResponse response = await request.send();
+      if (response.statusCode == 200) {
+        print('Response status: ${response.statusCode}');
+        return true;
+      } else {
+        print('Error: ${response.reasonPhrase}');
+        return false;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  DeletePosyandu(id) async {
+    try {
+      var request = http.Request(
+        'DELETE',
+        Uri.parse(base_url + '/posyandu/${id}'),
+      );
+      print("Request URL: ${request.url}");
+
+      http.StreamedResponse response = await request.send();
+      if (response.statusCode == 200) {
+        print('Response status: ${response.statusCode}');
+        return true;
+      } else {
+        print('Error: ${response.reasonPhrase}');
+        return false;
       }
     } catch (e) {
       print(e);

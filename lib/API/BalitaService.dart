3f8b9e6039e9d4517e00bb4dab35d8eb720fb534
Idasut17~ <yusufnuralam0@gmail.com~>
2025-07-05 23:01:42 +0700

    Perubahan ServiCe pada Balita penamabahan kaarakte

--- lib/API/BalitaService.dart
+++ lib/API/BalitaService.dart
@@ -4,58 +4,57 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Balitaservice {
-  CreateBalita(
-    nama,
-    nik,
-    tanggalLahir,
-    alamat,
-    jenisKelamin,
-    posyanduId,
-    bukuKIA,
+  Future<void> CreateBalita(
+    String nama,
+    String nik,
+    DateTime tanggalLahir,
+    String alamat,
+    String jenisKelamin,
+    int posyanduId,
+    String namaIbu,
+    String bukuKIA,
   ) async {
     try {
-      var request = http.Request('POST', Uri.parse(base_url + '/balita'));
-      request.body = json.encode({
-        "nama": nama,
-        "nik": nik,
-        "tanggal_lahir": tanggalLahir.toIso8601String(),
-        "alamat": alamat,
-        "jenis_kelamin": jenisKelamin,
-        "posyandu_id": posyanduId,
-        "Buku_KIA": bukuKIA,
-      });
-      request.headers.addAll({'Content-Type': 'application/json'});
+      final response = await http.post(
+        Uri.parse('$base_url/balita'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          "nama": nama,
+          "nik": nik,
+          "tanggal_lahir": tanggalLahir.toIso8601String().split('T')[0],
+          "alamat": alamat,
+          "jenis_kelamin": jenisKelamin,
+          "posyandu_id": posyanduId,
+          "nama_ibu": namaIbu,
+          "Buku_KIA": bukuKIA,
+        }),
+      );
 
-      http.StreamedResponse response = await request.send();
-      if (response.statusCode == 200) {
-        print('Response status: ${response.statusCode}');
-        return true;
-      } else {
-        print('Error: ${response.reasonPhrase}');
-        return false;
+      // HTTP 201 Created adalah standar untuk POST yang berhasil
+      if (response.statusCode != 201 && response.statusCode != 200) {
+        print('Error Body: ${response.body}');
+        throw Exception('Gagal membuat data balita: ${response.reasonPhrase}');
       }
+      print('Data balita berhasil dibuat.');
     } catch (e) {
       print(e);
-      return false;
+      throw Exception('Gagal terhubung ke server: $e');
     }
   }
 
-  GetBalitaData(id) async {
+  Future<BalitaModel> GetBalitaData(int id) async {
     try {
-      var request = http.Request('GET', Uri.parse(base_url + '/balita/${id}'));
-      http.StreamedResponse response = await request.send();
+      final response = await http.get(Uri.parse('$base_url/balita/$id'));
+
       if (response.statusCode == 200) {
-        print('Response status: ${response.statusCode}');
-        var responseData = await response.stream.bytesToString();
-        Map<String, dynamic> responDecode = json.decode(responseData);
-        BalitaList.clear();
-        BalitaList.add(BalitaModel.fromJson(responDecode['data']));
+        Map<String, dynamic> responseDecode = json.decode(response.body);
+        return BalitaModel.fromJson(responseDecode['data']);
       } else {
-        print('Error: ${response.reasonPhrase}');
-        return null;
+        throw Exception('Gagal memuat data balita: ${response.reasonPhrase}');
       }
     } catch (e) {
       print(e);
+      throw Exception('Gagal terhubung ke server: $e');
     }
   }
 
@@ -69,7 +68,6 @@ class Balitaservice {
         print('Response status: ${response.statusCode}');
         Map<String, dynamic> responDecode = json.decode(response.body);
         List<dynamic> data = responDecode['data'];
-        print("Data: $data");
 
         // Buat list dari data response dan kembalikan
         List<BalitaModel> balitaList =
@@ -86,62 +84,59 @@ class Balitaservice {
     }
   }
 
-  DeleteBalita(id) async {
+  Future<void> DeleteBalita(int id) async {
     try {
-      var request = http.Request(
-        'DELETE',
-        Uri.parse(base_url + '/balita/${id}'),
-      );
-      print("Request URL: ${request.url}");
+      final response = await http.delete(Uri.parse('$base_url/balita/$id'));
 
-      http.StreamedResponse response = await request.send();
-      if (response.statusCode == 200) {
-        print('Response status: ${response.statusCode}');
-        return true;
-      } else {
-        print('Error: ${response.reasonPhrase}');
-        return false;
+      if (response.statusCode != 200) {
+        throw Exception(
+          'Gagal menghapus data balita: ${response.reasonPhrase}',
+        );
       }
+      print('Data balita berhasil dihapus.');
     } catch (e) {
       print(e);
-      return false;
+      throw Exception('Gagal terhubung ke server: $e');
     }
   }
 
-  UpdateBalita(
-    id,
-    nama,
-    nik,
-    tanggalLahir,
-    alamat,
-    jenisKelamin,
-    posyanduId,
-    bukuKIA,
+  Future<void> UpdateBalita(
+    int id,
+    String nama,
+    String nik,
+    DateTime tanggalLahir,
+    String alamat,
+    String jenisKelamin,
+    int posyanduId,
+    String namaIbu,
+    String bukuKIA,
   ) async {
     try {
-      var request = http.Request('PUT', Uri.parse(base_url + '/balita/${id}'));
-      request.body = json.encode({
-        "nama": nama,
-        "nik": nik,
-        "tanggal_lahir": tanggalLahir.toIso8601String(),
-        "alamat": alamat,
-        "jenis_kelamin": jenisKelamin,
-        "posyandu_id": posyanduId,
-        "Buku_KIA": bukuKIA,
-      });
-      request.headers.addAll({'Content-Type': 'application/json'});
+      final response = await http.put(
+        Uri.parse('$base_url/balita/$id'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          "nama": nama,
+          "nik": nik,
+          "tanggal_lahir": tanggalLahir.toIso8601String().split('T')[0],
+          "alamat": alamat,
+          "jenis_kelamin": jenisKelamin,
+          "posyandu_id": posyanduId,
+          "nama_ibu": namaIbu,
+          "Buku_KIA": bukuKIA,
+        }),
+      );
 
-      http.StreamedResponse response = await request.send();
-      if (response.statusCode == 200) {
-        print('Response status: ${response.statusCode}');
-        return true;
-      } else {
-        print('Error: ${response.reasonPhrase}');
-        return false;
+      if (response.statusCode != 200) {
+        print('Error Body: ${response.body}');
+        throw Exception(
+          'Gagal memperbarui data balita: ${response.reasonPhrase}',
+        );
       }
+      print('Data balita berhasil diperbarui.');
     } catch (e) {
       print(e);
-      return false;
+      throw Exception('Gagal terhubung ke server: $e');
     }
   }
 }
